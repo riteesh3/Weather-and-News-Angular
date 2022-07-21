7cf86e697b0678fb87479e194175ef0d6b445505
Riteesh <riteeshreddy333@gmail.com>
2022-07-21 19:35:41 +0530

    Initial template commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather/weather.component';
+import { NewsComponent } from './news/news.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent,
+    NewsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
